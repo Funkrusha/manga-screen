8c68588047058c5f98b2b45d93e853d00d77fbd4
Elias <elias@iagent.no>
2013-07-05 15:55:11 +0200

    Øyvind is starting capacitive touch development

--- MSP430/main.c
+++ MSP430/main.c
@@ -2,8 +2,8 @@
 
 
 
-#include <io.h>
-#include <signal.h>
+#include <msp430.h>
+#include <legacymsp430.h>
 
 #define PIN_RESET 	BIT0
 #define TXD 		BIT1
@@ -13,25 +13,37 @@
 #define PIN_MOSI  	BIT6
 #define PIN_MISO  	BIT7
 
+#define CMP_NOP 		0x00
+#define CMD_SWRESET 	0x01
+#define CMD_RDRED 		0x06
+#define CMD_RDGREEN 	0x07
+#define CMD_RDBLUE  	0x08
+#define CMD_RDDPM   	0x0A
+#define CMD_RDDMADCTL 	0x0B
+#define CMD_RDDCOLMOD   0x0C
+
 #define LCD_C     0
 #define LCD_D     1
 
-#define CS_ACTIVE  1
-#define CS_ 0
-
 #define Bit_time 104 	// 9600 Baud, SMCLK=1MHz (1MHz/9600)=104
 #define Bit_time_5 52 	// Time for half a bit.
+
+#define TO_HEX(i) (i <= 9 ? '0' + i : 'A' - 10 + i)
+
 
 unsigned char BitCnt; 	// Bit count, used when transmitting byte
 unsigned int TXByte; 	// Value sent over UART when Transmit() is called
 
 // Function Definitions
 void LcdInitialise(void);
+void LcdTest(void);
 void LcdWrite(char dnc, char data, char cs);
 char LcdRead(char cs);
 void bl_init();
 void delay_ms(unsigned int n);			// Delay milliseconds.
-void Transmit(void);
+void Transmit(char c);
+void send(char* str);
+void hex(char x);
 static void __inline__ _brief_pause(register unsigned int n);
 
 void LcdInitialise(void){
@@ -48,13 +60,122 @@ void LcdInitialise(void){
 	LcdWrite(LCD_D, 0x63, 0);
 	P1OUT |= PIN_NCS; // Select Chip Enable High
 
-	P1OUT &= ~PIN_NCS; // Select Chip Enable low
-	LcdWrite(LCD_C, 0xDB, 0);
-	TXByte = LcdRead(1);
+	send("RDID2:");
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, 0xDB, 0); 
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
 
-	Transmit();
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, 0x3A, 0); // Interface pixel format
+	LcdWrite(LCD_D, 0x50, 0); // 16 bit
+	P1OUT |= PIN_NCS; 
+
+	//P1OUT &= ~PIN_NCS; 
+	//LcdWrite(LCD_C, 0xB3, 0); // Set RGB interface related register (B3h)
+	//LcdWrite(LCD_D, 0x00, 0); // EPL + VSPL + HSPL + DPL
+	//P1OUT |= PIN_NCS; 
+
+
+	delay_ms(130);
+
 	LcdWrite(LCD_C, 0x29, 1); // Display on command
 }
+
+void LcdTest(void){
+	char data;
+
+	send("Display power mode: ");
+	P1OUT &= ~PIN_NCS; // Select Chip Enable low
+	LcdWrite(LCD_C, 0x0A, 0);
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
+	if(data & (1<<7))
+		send("\tBooster OK\n");
+	else
+		send("\tBooster Error\n");
+	if(data & (1<<4))
+		send("\tSleep out mode\n");
+	else
+		send("\tSleep in mode\n");
+	if(data & (1<<3))
+		send("\tDisplay mode normal\n");
+	else
+		send("\tDisplay mode not normal\n");
+	if(data & (1<<2))
+		send("\tDisplay is on\n");
+	else
+		send("\tDisplay is off\n");
+
+	send("Display pixel format: ");
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, 0x0C, 0);
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
+	if(data == 0x70)
+		send("\t24 bit pixel format\n");
+	else if(data == 0x50)
+		send("\t16 bit pixel format\n");
+	else if(data == 0x60)
+		send("\t18 bit pixel format\n");
+	else
+		send("\tUnknown pixel format\n");
+
+	send("Display image mode: ");
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, 0x0D, 0);
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
+
+	send("Display signal mode: ");
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, 0x0E, 0);
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
+
+	send("Display self diagnostic: ");
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, 0x0F, 0);
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
+	if(data & (1<<7))
+		send("\tOTP match\n");
+	else
+		send("\tOTP mismatch\n");
+	if(data & (1<<6))
+		send("\tBooster level and timings OK\n");
+	else
+		send("\tBooster shit error\n");
+
+	
+
+
+	send("RDRED: ");
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, CMD_RDRED, 0);
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
+	send("RDBLUE: ");
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, CMD_RDBLUE, 0);
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
+	send("RDGREEN: ");
+	P1OUT &= ~PIN_NCS; 
+	LcdWrite(LCD_C, CMD_RDGREEN, 0);
+	data = LcdRead(1);
+	hex(data);
+	send("\n");
+}
+
 
 // Write data to the display
 void LcdWrite(char dnc, char data, char cs){
@@ -112,13 +233,14 @@ void bl_init(){
 }
 
 int main(void){
+	char data;
 	WDTCTL = WDTPW + WDTHOLD;               // Stop WDT
 
 	BCSCTL1 = CALBC1_1MHZ; // Set range
 	DCOCTL = CALDCO_1MHZ; // SMCLK = DCO = 1MHz
 
-	P1SEL |= TXD;
-	P1DIR |= PIN_RESET | TXD;
+	P1SEL |= TXD | PIN_BL;
+	P1DIR |= PIN_RESET | TXD | PIN_BL;
 	P1DIR |= PIN_NCS;
 	P1DIR |= PIN_MOSI;
 	P1DIR |= PIN_SCLK;
@@ -126,35 +248,36 @@ int main(void){
 	P1OUT |= PIN_RESET;
 	delay_ms(1000); // Wait for power to become stable.
 	delay_ms(1000); // Wait for power to become stable.
-	delay_ms(1000); // Wait for power to become stable.
-	delay_ms(1000); // Wait for power to become stable.
-	delay_ms(1000); // Wait for power to become stable.
-	delay_ms(1000); // Wait for power to become stable.
+	//delay_ms(1000); // Wait for power to become stable.
+	//delay_ms(1000); // Wait for power to become stable.
+	//delay_ms(1000); // Wait for power to become stable.
+	//delay_ms(1000); // Wait for power to become stable.
 
 	P1OUT &= ~PIN_RESET;
-	delay_ms(1); // Page 13 in manual for LCD
+	delay_ms(10); // Page 13 in manual for LCD
 	P1OUT |= PIN_RESET;
-	delay_ms(10);
+	delay_ms(100);
 
 	__bis_SR_register(GIE); 	// interrupts enabled
 
-	TXByte = 'O';
-	Transmit();
+	send("Start\n");
+	LcdTest();
 	LcdInitialise();
-	TXByte = 'D';
-	Transmit();
+	LcdTest();
+	send("Done\n");
+
+	bl_init();
 
 	while(1){
-		P1OUT &= ~PIN_NCS; // Select Chip Enable low
-		LcdWrite(LCD_C, 0xDB, 0);
-		TXByte = LcdRead(1); // Read and CS high
-		Transmit();
-		delay_ms(10);
+		delay_ms(10000);
+		//LcdTest();
+		//bl_init();
 	}
 }
 
 // Function Transmits Character from TXByte
-void Transmit(){
+void Transmit(char c){
+	TXByte = c;
 
 	CCTL0 = OUT; // TXD Idle as Mark
 	TACTL = TASSEL_2 + MC_2; // SMCLK, continuous mode
@@ -170,6 +293,17 @@ void Transmit(){
 	while ( CCTL0 & CCIE ); // Wait for previous TX completion
 }
 
+void send(char* str){
+	while(*str != '\0')
+		Transmit(*str++);
+}
+
+void hex(char x){
+	Transmit('0');
+	Transmit('x');
+	Transmit( TO_HEX(((x & 0x00F0) >> 4)) );
+	Transmit( TO_HEX(((x & 0x000F) >> 0)) );
+}
 interrupt(TIMERA0_VECTOR) ta1_isr (void){
 	CCR0 += Bit_time; 					// Add Offset to CCR0
 	if ( BitCnt == 0){ 					// If all bits TXed
